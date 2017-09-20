71df662c8e5b6eee983c16e9d7f14c4ae7a65177
Adam Thompson <adam@serialphotog.com>
2017-09-20 15:10:38 -0400

    Removed unused dependencies

--- Markdown Buddy/MainForm.cs
+++ Markdown Buddy/MainForm.cs
@@ -1,12 +1,5 @@
 ﻿using Markdown_Buddy.Core;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Markdown_Buddy
