dbe52b6c4cc673b6e56092d15329aa5d5f804dcf
Adam Thompson <adam@serialphotog.com>
2017-09-23 14:42:24 -0400

    Fixed resize properties for settings dialog

--- Markdown Buddy/Core/SettingsForm.Designer.cs
+++ Markdown Buddy/Core/SettingsForm.Designer.cs
@@ -31,20 +31,21 @@
             this.btnSave = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
             this.groupBoxFonts = new System.Windows.Forms.GroupBox();
+            this.lblFontStyleValue = new System.Windows.Forms.Label();
+            this.lblFontStyle = new System.Windows.Forms.Label();
+            this.lblFontSizeValue = new System.Windows.Forms.Label();
+            this.lblFontSize = new System.Windows.Forms.Label();
             this.lblFontValue = new System.Windows.Forms.Label();
             this.lblFont = new System.Windows.Forms.Label();
             this.btnSelectFont = new System.Windows.Forms.Button();
             this.fontDialog = new System.Windows.Forms.FontDialog();
-            this.lblFontSize = new System.Windows.Forms.Label();
-            this.lblFontSizeValue = new System.Windows.Forms.Label();
-            this.lblFontStyle = new System.Windows.Forms.Label();
-            this.lblFontStyleValue = new System.Windows.Forms.Label();
             this.groupBoxFonts.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(246, 383);
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(247, 176);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(75, 23);
             this.btnSave.TabIndex = 0;
@@ -54,7 +55,8 @@
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(165, 383);
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.Location = new System.Drawing.Point(166, 176);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(75, 23);
             this.btnCancel.TabIndex = 1;
@@ -78,6 +80,42 @@
             this.groupBoxFonts.TabIndex = 2;
             this.groupBoxFonts.TabStop = false;
             this.groupBoxFonts.Text = "Font";
+            // 
+            // lblFontStyleValue
+            // 
+            this.lblFontStyleValue.AutoSize = true;
+            this.lblFontStyleValue.Location = new System.Drawing.Point(66, 84);
+            this.lblFontStyleValue.Name = "lblFontStyleValue";
+            this.lblFontStyleValue.Size = new System.Drawing.Size(91, 13);
+            this.lblFontStyleValue.TabIndex = 7;
+            this.lblFontStyleValue.Text = "Current Font Style";
+            // 
+            // lblFontStyle
+            // 
+            this.lblFontStyle.AutoSize = true;
+            this.lblFontStyle.Location = new System.Drawing.Point(3, 84);
+            this.lblFontStyle.Name = "lblFontStyle";
+            this.lblFontStyle.Size = new System.Drawing.Size(57, 13);
+            this.lblFontStyle.TabIndex = 6;
+            this.lblFontStyle.Text = "Font Style:";
+            // 
+            // lblFontSizeValue
+            // 
+            this.lblFontSizeValue.AutoSize = true;
+            this.lblFontSizeValue.Location = new System.Drawing.Point(66, 56);
+            this.lblFontSizeValue.Name = "lblFontSizeValue";
+            this.lblFontSizeValue.Size = new System.Drawing.Size(88, 13);
+            this.lblFontSizeValue.TabIndex = 5;
+            this.lblFontSizeValue.Text = "Current Font Size";
+            // 
+            // lblFontSize
+            // 
+            this.lblFontSize.AutoSize = true;
+            this.lblFontSize.Location = new System.Drawing.Point(6, 56);
+            this.lblFontSize.Name = "lblFontSize";
+            this.lblFontSize.Size = new System.Drawing.Size(54, 13);
+            this.lblFontSize.TabIndex = 3;
+            this.lblFontSize.Text = "Font Size:";
             // 
             // lblFontValue
             // 
@@ -113,50 +151,16 @@
             this.fontDialog.AllowVerticalFonts = false;
             this.fontDialog.ShowEffects = false;
             // 
-            // lblFontSize
-            // 
-            this.lblFontSize.AutoSize = true;
-            this.lblFontSize.Location = new System.Drawing.Point(6, 56);
-            this.lblFontSize.Name = "lblFontSize";
-            this.lblFontSize.Size = new System.Drawing.Size(54, 13);
-            this.lblFontSize.TabIndex = 3;
-            this.lblFontSize.Text = "Font Size:";
-            // 
-            // lblFontSizeValue
-            // 
-            this.lblFontSizeValue.AutoSize = true;
-            this.lblFontSizeValue.Location = new System.Drawing.Point(66, 56);
-            this.lblFontSizeValue.Name = "lblFontSizeValue";
-            this.lblFontSizeValue.Size = new System.Drawing.Size(88, 13);
-            this.lblFontSizeValue.TabIndex = 5;
-            this.lblFontSizeValue.Text = "Current Font Size";
-            // 
-            // lblFontStyle
-            // 
-            this.lblFontStyle.AutoSize = true;
-            this.lblFontStyle.Location = new System.Drawing.Point(3, 84);
-            this.lblFontStyle.Name = "lblFontStyle";
-            this.lblFontStyle.Size = new System.Drawing.Size(57, 13);
-            this.lblFontStyle.TabIndex = 6;
-            this.lblFontStyle.Text = "Font Style:";
-            // 
-            // lblFontStyleValue
-            // 
-            this.lblFontStyleValue.AutoSize = true;
-            this.lblFontStyleValue.Location = new System.Drawing.Point(66, 84);
-            this.lblFontStyleValue.Name = "lblFontStyleValue";
-            this.lblFontStyleValue.Size = new System.Drawing.Size(91, 13);
-            this.lblFontStyleValue.TabIndex = 7;
-            this.lblFontStyleValue.Text = "Current Font Style";
-            // 
             // SettingsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(333, 418);
+            this.ClientSize = new System.Drawing.Size(334, 211);
             this.Controls.Add(this.groupBoxFonts);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnSave);
+            this.MaximumSize = new System.Drawing.Size(350, 250);
+            this.MinimumSize = new System.Drawing.Size(350, 250);
             this.Name = "SettingsForm";
             this.Text = "Markdown Buddy Settings";
             this.groupBoxFonts.ResumeLayout(false);
